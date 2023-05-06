2ab418ed00f9216cd1b9f2858858eab9b74b79e8
Averito <avenantmar@gmail.com>
2023-05-06 13:45:57 +0300

    update types in index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,26 @@ import {
 	getAnilibriaSchedule,
 	getAnilibriaTeam
 } from './api/methods'
+import type { GetAnilibriaTitleQueryParams } from './api/methods/get-anilibria-title'
+import type { GetAnilibriaTitlesQueryParams } from './api/methods/get-anilibria-titles'
+import type { GetAnilibriaUpdatesQueryParams } from './api/methods/get-anilibria-updates'
+import type { GetAnilibriaChangesQueryParams } from './api/methods/get-anilibria-changes'
+import type { GetAnilibriaScheduleQueryParams } from './api/methods/get-anilibria-schedule'
+import type { GetAnilibriaRandomTitleQueryParams } from './api/methods/get-anilibria-random-title'
+import type { GetAnilibriaYouTubeQueryParams } from './api/methods/get-anilibria-youtube'
+import type {
+	GetAnilibriaFeedQueryParams,
+	GetAnilibriaFeedReturn
+} from './api/methods/get-anilibria-feed'
+import type {
+	GetAnilibriaSeedStatsQueryParams,
+	GetAnilibriaSeedStatsReturn
+} from './api/methods/get-anilibria-seed-stats'
+import type { GetAnilibriaRssQueryParams } from './api/methods/get-anilibria-rss'
+import type { AnilibriaSearchTitlesQueryParams } from './api/methods/anilibria-search-titles'
+import type { AnilibriaAdvancedSearchQueryParams } from './api/methods/anilibria-advanced-search'
+import type { GetAnilibriaTeamReturn } from './api/methods/get-anilibria-team'
+import { AnilibriaOptions } from './api/anilibria.types'
 import type {
 	Title,
 	YouTube,
@@ -41,7 +61,23 @@ export type {
 	Torrent,
 	Rating,
 	Series,
-	ObjectSeries
+	ObjectSeries,
+	AnilibriaAdvancedSearchQueryParams,
+	AnilibriaSearchTitlesQueryParams,
+	GetAnilibriaTeamReturn,
+	GetAnilibriaFeedReturn,
+	GetAnilibriaSeedStatsQueryParams,
+	GetAnilibriaSeedStatsReturn,
+	GetAnilibriaFeedQueryParams,
+	GetAnilibriaYouTubeQueryParams,
+	GetAnilibriaRandomTitleQueryParams,
+	GetAnilibriaScheduleQueryParams,
+	GetAnilibriaChangesQueryParams,
+	GetAnilibriaUpdatesQueryParams,
+	GetAnilibriaRssQueryParams,
+	GetAnilibriaTitleQueryParams,
+	GetAnilibriaTitlesQueryParams,
+	AnilibriaOptions
 }
 export {
 	ANILIBRIA_API_URI_BY_DEFAULT,
